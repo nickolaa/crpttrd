2bc77e9a27929c2b4d9a525b72fb35b6c432a7b4
Nikolai Iarina <nickolaa@gmail.com>
2018-02-13 22:39:06 +0300

    main settings correction

--- main_settings.py
+++ main_settings.py
@@ -1,10 +1,10 @@
 default_loss_time = 14  # in days
 min_volume = 50  # min volume возможно стоит пересматривать
 min_ask = 0.00005  # min ask
-min_profit = 0.01
+min_profit = 0.014
 max_profit = 0.5
 min_delta = 0.02
 min_order_mult = 1.5
-buy_step = 0.00000001
-sell_step = 0.00000001
+buy_step = 0.00000003
+sell_step = 0.00000003
 max_dev = 0.25
